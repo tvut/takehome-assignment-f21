ad7048f50c8ff9076571bc777fd3bde16b71134a
Tristan Stevens <tristan@catzooks.com>
2021-09-17 17:58:34 -0400

    finished vue-frontend

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,12 +1,17 @@
 <template>
-  <p>I'm a Counter!</p>
+  <div>
+    <p>Counter is currently at {{count}}</p>
+    <button v-on:click="count += 1">Increase Count</button>
+    <button v-on:click="count -= 1">Decrease Count</button>
+  </div>
 </template>
 
 <script>
 export default {
+  props:['count'],
   data() {
     return {
-      count: 0
+      count
     };
   }
 };
--- vue-frontend/src/components/Show.vue
+++ vue-frontend/src/components/Show.vue
@@ -1,11 +1,18 @@
 <template>
   <!-- PART 4: Add your code here -->
-  <div></div>
+  <div>
+    <p>Show Name: {{name}}</p>
+    <Counter :count = episodes_seen />
+  </div>
 </template>
 
 <script>
 // PART 4: Add your code here
-export default {};
+import Counter from "./Counter.vue"
+export default {
+  components:{Counter},
+  props: ['id', 'name', 'episodes_seen']
+};
 </script>
 
 <style>
